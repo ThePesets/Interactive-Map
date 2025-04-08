1dd8434e6317e0025fb5f6742ac7a6865fc77b9f
Veronika <tretyakova.vs.12@gmail.com>
2025-04-08 21:16:09 +0300

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+def info_decemb(request):
+    return render(request, 'main/info_decemb.html')
+def info_borod(request):
+    return render(request, 'main/info_borod.html')
+def info_polt(request):
+    return render(request, 'main/info_polt.html')
+def info_plague(request):
+    return render(request, 'main/info_plague.html')
+def info_salt(request):
+    return render(request, 'main/info_salt.html')
+def info_solovki(request):
+    return render(request, 'main/info_solovki.html')
+def info_razin(request):
+    return render(request, 'main/info_razin.html')
+def info_copper(request):
+    return render(request, 'main/info_copper.html')
+def info_gangut(request):
+    return render(request, 'main/info_gangut.html')
+def info_pugach(request):
+    return render(request, 'main/info_pugach.html')
+def info_izmail(request):
+    return render(request, 'main/info_izmail.html')
+def info_sevast(request):
+    return render(request, 'main/info_sevast.html')
+def info_shamil(request):
+    return render(request, 'main/info_shamil.html')
+def info_sinop(request):
+    return render(request, 'main/info_sinop.html')
+def info_brus(request):
+    return render(request, 'main/info_brus.html')
+def info_octob(request):
+    return render(request, 'main/info_octob.html')
+def info_stalg(request):
+    return render(request, 'main/info_stalg.html')
+def info_tsush(request):
+    return render(request, 'main/info_tsush.html')
+def info_smolnsk(request):
+    return render(request, 'main/info_smolnsk1609.html')
+def info_smolnsk1609(request):
+    return render(request, 'main/info_smolnsk1812.html')
+def info_klush(request):
+    return render(request, 'main/info_klush.html')
+def info_chigir(request):
+    return render(request, 'main/info_chigir.html')
+def info_azov(request):
+    return render(request, 'main/info_azov.html')
+def info_grsegs(request):
+    return render(request, 'main/info_grsegs.html')
+def info_lesn(request):
+    return render(request, 'main/info_lesn.html')
+def info_maloyr(request):
+    return render(request, 'main/info_maloyr.html')
+def info_chesm(request):
+    return render(request, 'main/info_chesm.html')
+def info_tarut(request):
+    return render(request, 'main/info_tarut.html')
+def info_leipz(request):
+    return render(request, 'main/info_leipz.html')
+def info_smolnsk1812(request):
+    return render(request, 'main/info_smolnsk1812.html')
+def info_aust(request):
+    return render(request, 'main/info_aust.html')
+def info_berl(request):
+    return render(request, 'main/info_berl.html')
+def info_blokl(request):
+    return render(request, 'main/info_blokl.html')
+def info_rzhev(request):
+    return render(request, 'main/info_rzhev.html')
+def info_galic(request):
+    return render(request, 'main/info_galic.html')
